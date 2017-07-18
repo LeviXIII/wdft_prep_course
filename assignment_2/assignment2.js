eecc64e89f95b364d845071527f50e739a04b80d
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2017-07-18 19:13:16 -0400

    Finished assignment 2.

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,8 +2,10 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+for (var i = 1; i <= 100; i++)
+    console.log(i);
 
-
+console.log("");        //Used to create spacing.
 
 /*
 Exercise #2
@@ -17,3 +19,15 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+/* This method uses concatenation of a string var. */
+var pound = "#";        //Symbol string
+var i = 0;              //Counter
+
+while(i < 7)
+{
+    console.log(pound);
+    pound = pound + "#"
+    i++;
+}
+console.log("\n");
